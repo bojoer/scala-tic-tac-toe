10ad116ee80018a9c9b35a2c9d1045971807a7fc
Mark Dessain <mark.dessain@gmail.com>
2014-04-01 21:54:24 +0100

    Travis ftp deployment

--- ftp_deploy.py
+++ ftp_deploy.py
@@ -1,17 +1,36 @@
 import os
+import glob
 import ftplib
 
-FTP_HOST = os.environ.get('CI_FTP_HOST', 'apps.markdessain.com')
-FTP_USER = os.environ.get('CI_FTP_USER', 'm')
-FTP_PASS = os.environ.get('CI_FTP_PASS', 'm')
+FTP_HOST = os.environ.get('FTP_HOST')
+FTP_USER = os.environ.get('FTP_USER')
+FTP_PASS = os.environ.get('FTP_PASS')
 
-CI_BUILD = os.environ.get('TRAVIS_BUILD_NUMBER', '1')
+CI_BUILD = os.environ.get('TRAVIS_BUILD_NUMBER')
 
 ftp = ftplib.FTP(FTP_HOST)
 ftp.login(FTP_USER, FTP_PASS)
 
-#filename = 'target/scala-2.10/tictactoe-1.0.jar'
-filename = 'README.md'
+release_directory = "/files/%s" % CI_BUILD
 
-ftp.cwd("/files")
-ftp.storlines("STOR deploy-%s.jar" % CI_BUILD, open(filename, 'r'))
+try:
+    ftp.delete(release_directory)
+    print "Deleted directory: %s" % release_directory
+except ftplib.error_perm:
+    print "Unable to delete directory: %s" % release_directory
+
+try:
+    ftp.mkd(release_directory)
+    print "Made directory: %s" % release_directory
+except ftplib.error_perm:
+    print "Unable to make directory: %s" % release_directory
+
+ftp.cwd("/files/%s" % CI_BUILD)
+file_paths = glob.glob('target/scala-*/*.jar')
+
+for file_path in file_paths:
+
+    file_name = file_path.split("/")[-1]
+
+    ftp.storlines("STOR %s" % file_name, open(file_path, 'r'))
+    print "Written file: %s" % file_path
